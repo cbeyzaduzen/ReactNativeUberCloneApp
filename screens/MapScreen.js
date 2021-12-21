2a015e453de8e45577dc2799b56ed159495f48b6
Yusuf <yusuf.isik@bb.com.tr>
2021-12-21 16:59:40 +0300

    first commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const MapScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Map</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default MapScreen;
